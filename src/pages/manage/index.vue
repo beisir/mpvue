3c283e94f80bb98312bb26afbe5da7a863b4effa
beisir <hsy7426@163.com>
2018-06-11 00:07:35 +0800

    mup+koa

---
+++ src/pages/manage/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <div>
+        <navigator url="/pages/index/main">跳转页面</navigator>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: {
+    hello: 'hello wrodvvvvv'
+  },
+  methods: {
+    async fn () {
+      const index = await wx.request({
+        url: 'https://wsprod.hc360.com/get/prodbytime?pageSize=10&pageNo=0',
+        success (options) {
+          console.log(options)
+        }
+      })
+      return index
+    }
+  },
+  created () {
+    this.fn().then((options) => {
+      console.log(options)
+    })
+  }
+
+}
+</script>
+
+<style lang="css">
+</style>
